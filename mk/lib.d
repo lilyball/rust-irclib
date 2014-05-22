cd6124669cf8131c08d24b8305676c779d1a6e09
Kevin Ballard <kevin@sb.org>
2014-05-22 10:15:41 -0700

    Fix source tree for new module location rules
    Rename conn.rs -> conn/mod.rs and move handlers.rs into conn/.

--- mk/lib.d
+++ mk/lib.d
@@ -1,4 +1,4 @@
-libirc-ad3d5237-0.1.rlib: src/lib.rs src/conn.rs src/handlers.rs
-doc: src/lib.rs src/conn.rs src/handlers.rs
-mk/lib.d: src/lib.rs src/conn.rs src/handlers.rs
+libirc-ad3d5237-0.1.rlib: src/lib.rs src/conn/mod.rs src/conn/handlers.rs
+doc: src/lib.rs src/conn/mod.rs src/conn/handlers.rs
+mk/lib.d: src/lib.rs src/conn/mod.rs src/conn/handlers.rs
 
--- mk/test.d
+++ mk/test.d
@@ -1,3 +1,3 @@
-test-irc: src/lib.rs src/conn.rs src/handlers.rs
-mk/test.d: src/lib.rs src/conn.rs src/handlers.rs
+test-irc: src/lib.rs src/conn/mod.rs src/conn/handlers.rs
+mk/test.d: src/lib.rs src/conn/mod.rs src/conn/handlers.rs
 
